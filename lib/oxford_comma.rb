27fff69d7cb7930a0535ddc809982944d657e51c
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-14 22:28:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,8 @@
 
 def oxford_comma(array)
-  if array.count > 2
-    array.each do |comma|
-      comma << ","
-    end
-  end
-  array.join(" and ")
+  array.join(" and ") if array.size < 3
+  array[-1] = "and " + array.[-1]
+  array.join(", ")
+
+
 end
